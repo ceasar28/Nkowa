6f807a75883521d8c178038363c6784979fac870
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-04-17 23:26:39 +0100

    edited welcome message

--- nkowa/src/bot/Markups/welcome.ts
+++ nkowa/src/bot/Markups/welcome.ts
@@ -1,6 +1,6 @@
 export const welcomeMessageMarkup = async (userName: string) => {
   return {
-    message: `Hi ${userName}, kedu! 👋, Welcome to Nkọwa, your go to Retrieval-augmented generation bot. Here is what I can do:\n\n– Help you summarize any pdf 📄.\n– Interactive Conversation 💬 with your PDF.\n– Generate unique 🌟 content out of your pdf.\n\nShall we start? 👇`,
+    message: `Hi ${userName}, kedu! 👋, Welcome to Nkọwa, your go to Retrieval-augmented generation bot. Here is what I can do:\n\n– Help you summarize any Textbook | Article | Research works pdf 📄.\n– Interactive Conversation 💬 with your PDF.\n– Generate unique 🌟 content out of your pdf.\n– I can also give you responses in any Language of your choice\n\nShall we start? 👇`,
     keyboard: [[{ text: 'Lets get started 🚀', callback_data: '/getStarted' }]],
   };
 };
